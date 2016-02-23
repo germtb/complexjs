4937a01288a78e824eda68d27d5be1d821b41b08
germtb <germtf@gmail.com>
2016-02-23 01:01:25 +0000

    Change anonymous functions to named functions

--- src/complex.js
+++ src/complex.js
@@ -1,147 +1,147 @@
 
-export const isEuler = function(c) {
+export function isEuler(c) {
   return 're' in c || 'im' in c;
-};
+}
 
-export const isPolar = function(c) {
+export function isPolar(c) {
   return 'r' in c && 'arg' in c;
-};
+}
 
-export const re = function(c) {
+export function re(c) {
   return isEuler(c) ? re_euler(c) : re_polar(c);
-};
+}
 
-export const re_euler = function(c) {
+export function re_euler(c) {
   return c.re ? c.re : 0;
-};
+}
 
-export const re_polar = function(c) {
+export function re_polar(c) {
   return c.r * Math.cos(c.arg);
-};
+}
 
-export const im = function(c) {
+export function im(c) {
   return isEuler(c) ? im_euler(c) : im_polar(c);
-};
+}
 
-export const im_euler = function(c) {
+export function im_euler(c) {
   return c.im ? c.im : 0;
-};
+}
 
-export const im_polar = function(c) {
+export function im_polar(c) {
   return c.r * Math.sin(c.arg);
-};
+}
 
-export const csum = function(c1, c2) {
+export function csum(c1, c2) {
   return isEuler(c1) ? csum_euler(c1, toEuler(c2)) : csum_polar(c1, toPolar(c2));
-};
+}
 
-export const csum_euler = function(c1, c2) {
+export function csum_euler(c1, c2) {
   return {
     ...c2, ...c1,
     re: re(c1) + re(c2),
     im: im(c1) + im(c2)
-  };
-};
+  }
+}
 
-export const csum_polar = function(c1, c2) {
+export function csum_polar(c1, c2) {
   return {
     ...c2, ...c1,
     ...toPolar({re: re(c1) + re(c2), im: im(c1) + im(c2)})
-  };
-};
+  }
+}
 
-export const csub = function(c1, c2) {
+export function csub(c1, c2) {
   return isEuler(c1) ? csub_euler(c1, toEuler(c2)) : csub_polar(c1, toPolar(c2));
-};
+}
 
-export const csub_euler = function(c1, c2) {
+export function csub_euler(c1, c2) {
   return {
     ...c2, ...c1,
     re: re(c1) - re(c2),
     im: im(c1) - im(c2)
-  };
-};
+  }
+}
 
-export const csub_polar = function(c1, c2) {
+export function csub_polar(c1, c2) {
   return {
     ...c2, ...c1,
     ...toPolar({
       re: re(c1) - re(c2),
       im: im(c1) - im(c2)})
-  };
-};
+  }
+}
 
-export const cmul = function(c1, c2) {
+export function cmul(c1, c2) {
   return isEuler(c1) ? cmul_euler(c1, toEuler(c2)) : cmul_polar(c1, toPolar(c2));
-};
+}
 
-export const cmul_euler = function(c1, c2) {
+export function cmul_euler(c1, c2) {
   return {
     ...c2, ...c1,
     re: re(c1) * re(c2) - im(c1) * im(c2),
     im: re(c1) * im(c2) + re(c2) * im(c1)
-  };
-};
+  }
+}
 
-export const cmul_polar = function(c1, c2) {
+export function cmul_polar(c1, c2) {
   return {
     ...c2, ...c1,
     r: c1.r * c2.r,
     arg: normalize(c1.arg + c2.arg)
-  };
-};
+  }
+}
 
-export const cdiv = function(c1, c2) {
+export function cdiv(c1, c2) {
   return isEuler(c1) ? cdiv_euler(c1, toEuler(c2)) : cdiv_polar(c1, toPolar(c2));
-};
+}
 
-export const cdiv_euler = function(c1, c2) {
+export function cdiv_euler(c1, c2) {
   const mul = cmul(c1, conjugate(c2));
   const mod2 = cmod2(c2);
   return {
     ...c2, ...c1,
     re: mul.re / mod2,
     im: mul.im / mod2
-  };
-};
+  }
+}
 
-export const cdiv_polar = function(c1, c2) {
+export function cdiv_polar(c1, c2) {
   return {
     ...c2, ...c1,
     r: c1.r / c2.r,
     arg: normalize(c1.arg - c2.arg)
-  };
-};
+  }
+}
 
-export const cmod = function(c) {
+export function cmod(c) {
   return isEuler(c) ? cmod_euler(c) : cmod_polar(c);
-};
+}
 
-export const cmod_euler = function(c) {
+export function cmod_euler(c) {
   return Math.sqrt(re(c) * re(c) + im(c) * im(c));
-};
+}
 
-export const cmod_polar = function(c) {
+export function cmod_polar(c) {
   return c.r;
-};
+}
 
-export const cmod2 = function(c) {
+export function cmod2(c) {
   return isEuler(c) ? cmod2_euler(c) : cmod2_polar(c);
-};
+}
 
-export const cmod2_euler = function(c) {
+export function cmod2_euler(c) {
   return re(c) * re(c) + im(c) * im(c);
-};
+}
 
-export const cmod2_polar = function(c) {
+export function cmod2_polar(c) {
   return c.r * c.r;
-};
+}
 
-export const carg = function(c) {
+export function carg(c) {
   return isEuler(c) ? carg_euler(c) : carg_polar(c);
-};
+}
 
-export const carg_euler = function(c) {
+export function carg_euler(c) {
   const x = re(c);
   const y = im(c);
 
@@ -162,9 +162,9 @@ export const carg_euler = function(c) {
   } else if (x > 0 && y < 0) {
     return normalize(arg - Math.PI);
   }
-};
+}
 
-const normalize = function(a) {
+function normalize(a) {
   if (a > Math.PI) {
     return normalize(a - 2 * Math.PI);
   } else if (a <= - Math.PI) {
@@ -172,68 +172,68 @@ const normalize = function(a) {
   } else {
     return a;
   }
-};
+}
 
-export const carg_polar = function(c) {
+export function carg_polar(c) {
   return c.arg;
-};
+}
 
-export const toEuler = function(c) {
+export function toEuler(c) {
   return isEuler(c) ? c : toEuler_polar(c);
-};
+}
 
-export const toEuler_polar = function(c) {
+export function toEuler_polar(c) {
   return {
     ...c,
     r: undefined,
     arg: undefined,
     re: re_polar(c),
     im: im_polar(c)
-  };
-};
+  }
+}
 
-export const toPolar = function(c) {
+export function toPolar(c) {
   return isEuler(c) ? toPolar_euler(c) : c;
-};
+}
 
-export const toPolar_euler = function(c) {
+export function toPolar_euler(c) {
   return {
     ...c,
     re: undefined,
     im: undefined,
     r: cmod(c),
     arg: carg(c)
-  };
-};
+  }
+}
 
-export const conjugate = function(c) {
+export function conjugate(c) {
   return isEuler(c) ? conjugate_euler(c) : conjugate_polar(c);
-};
+}
 
-export const conjugate_euler = function(c) {
+export function conjugate_euler(c) {
   return {
     ...c,
     re: re(c),
     im: -im(c)
-  };
-};
+  }
+}
 
-export const conjugate_polar = function(c) {
+export function conjugate_polar(c) {
   return {
     ...c,
     r: c.r,
     arg: -c.arg
-  };
-};
+  }
+}
 
-export const cequals = function(c1, c2) {
+export function cequals(c1, c2) {
   return isEuler(c1) ? cequals_euler(c1, toEuler(c2)) : cequals_polar(c1, toPolar(c2));
-};
+}
 
-export const cequals_euler = function(c1, c2) {
+export function cequals_euler(c1, c2) {
   return c1.re === c2.re &&  c1.im === c2.im;
-};
+}
 
-export const cequals_polar = function(c1, c2) {
+export function cequals_polar(c1, c2) {
   return c1.r === c2.r && c1.arg === c2.arg;
-};
+}
--- src/geometry.js
+++ src/geometry.js
@@ -5,29 +5,29 @@ import {
   cmod
 } from './complex';
 
-export const translate = function(c, translation) {
+export function translate(c, translation) {
   return csum(c, translation);
-};
+}
 
-export const scale = function(c, factor, pivot = undefined) {
+export function scale(c, factor, pivot = undefined) {
   return pivot ?
     csum(cmul(csub(c, pivot), {re: factor}), pivot) :
     cmul(c, {re: factor});
-};
+}
 
-export const rotate = function(c, delta, pivot = undefined) {
+export function rotate(c, delta, pivot = undefined) {
   return pivot ?
     csum(cmul(csub(c, pivot), {r: 1, arg: delta }), pivot) :
     cmul(c, {r: 1, arg: delta });
-};
+}
 
-export const distance = function(c1, c2) {
+export function distance(c1, c2) {
   return cmod(csub(c1, c2));
-};
+}
 
-export const vector = function(x, y) {
+export function vector(x, y) {
   return {
     re: x,
     im: y
   };
-};
+}
